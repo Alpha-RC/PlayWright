00375e89245c2ef829bde3d9af93587def250329
Rohit <rohitchillal0722@gmail.com>
2026-05-07 12:53:15 +0530

    Update workspace changes

--- Chaptor01_JSBasics/01_Basics.js
+++ Chaptor01_JSBasics/01_Basics.js
@@ -1,5 +1,3 @@
-
-
 console.log("Welcome to learn Playwright with JavaScript!");
 // above code is used to print a message in the console, 
 // which is useful for debugging and logging purposes.
@@ -8,5 +6,6 @@ console.log("Welcome to learn Playwright with JavaScript!");
 let x = 10; //Storing value in x variable, this variable value is can be 
 // changed later in the code, so we can say that this variable is mutable.
 
-const y = 20; //Storing value in y variable, this variable value is cannot be changed later in the code, 
+const y = 20; //Storing value in y variable, 
+// this variable value cannot be changed later in the code,
 // so we can say that this variable is immutable.
