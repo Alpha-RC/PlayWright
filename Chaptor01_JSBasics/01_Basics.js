92f64162bdaa2a5370c6ffea12c619d40b88e9f4
Rohit <rohitchillal0722@gmail.com>
2026-05-07 12:50:28 +0530

    Update 01_Basics.js

--- Chaptor01_JSBasics/01_Basics.js
+++ Chaptor01_JSBasics/01_Basics.js
@@ -1,3 +1,12 @@
-console.log("Welcome to learn Playwright with JavaScript!");
 
-let x = 10;
+
+console.log("Welcome to learn Playwright with JavaScript!");
+// above code is used to print a message in the console, 
+// which is useful for debugging and logging purposes.
+
+
+let x = 10; //Storing value in x variable, this variable value is can be 
+// changed later in the code, so we can say that this variable is mutable.
+
+const y = 20; //Storing value in y variable, this variable value is cannot be changed later in the code, 
+// so we can say that this variable is immutable.
