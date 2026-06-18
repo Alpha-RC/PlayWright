d8f53dc2dd0513b047ede03df3688dab78396691
Rohit <rohitchillal0722@gmail.com>
2026-06-18 22:39:26 +0530

    Async Await

--- Chaptor_16_Async_Await/06_IQ.js
+++ Chaptor_16_Async_Await/06_IQ.js
@@ -23,3 +23,25 @@ let status1 = Promise.resolve(200);
 console.log(status1); // Promise { 200 }
 
 //--------------------------------------------------------------------
+
+
+async function testFlow() {
+    
+    let step1 = await Promise.resolve("Opened Browser");
+    console.log(step1);
+
+    let step2 = await Promise.resolve("Clicked login");
+    console.log(step2);
+
+    let step3 = await Promise.resolve("Verified Dashboard");
+    console.log(step3);
+
+    let step4 = await Promise.resolve("Test Finished");
+    console.log(step4);
+}
+
+testFlow();
+
+// Above flow is sequential flow
+
+
